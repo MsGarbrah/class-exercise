c01a94fe3c1a5ae1e26e94e8532d07d7f948c1fd
Christabelle <christefani@yahoo.com>
2023-05-20 14:57:10 +0000

    added js

---
+++ script.js
@@ -0,0 +1,28 @@
+// arithmetic operators
+let x = 25;
+let y = 6;
+console.log (x+y);
+
+console.log (30.99 * -20.56);
+
+console.log (500/20)
+
+// comparison operators
+let num1 = 48;
+let num2 = 260;
+console.log (num2 < num1);
+
+console.log (27 > 12);
+
+console.log (57 >= 57);
+
+// logical operators
+let age1 = 21;
+let age2 = 37;
+let age3 = 28;
+let age4 = 31;
+console.log (age1 > age2 && age3 < age4);
+
+console.log (12 <= 16 && 92 > 77);
+
+console.log (45 > 63 || 27 == 26);
